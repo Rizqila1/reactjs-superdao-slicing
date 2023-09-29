a5ee8050b9ddb5ca8a8714c7abfd8db85f63cd68
Rizqila1 <ikilatr@gmail.com>
2023-09-29 22:14:27 +0700

    feat: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: { min: "360px", max: "899px" },
+      md: { min: "900px", max: "1199px" },
+      lg: { min: "1200px" },
+    },
+    extend: {
+      colors: {
+        customyellow: "#FFCF01",
+        darkyellow: "#FFC24C",
+        softyellow: "#DBE268",
+        custompink: "#FF9E9E",
+        palmpink: "#FFB4D3",
+        custompurple: "#D387F7",
+        palmpurple: "#CEAAFF",
+        softpurple: "#E2C1F9",
+        lightblue: "#77E6FF",
+        customblue: "#23C9DE",
+        darkblue: "#182632",
+        softblue: "#77E6FF",
+        customgrey: "#707F8D",
+        customorange: "#FFA800",
+        cardbg: "#121E28",
+        cardborder: "#2A3744",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
